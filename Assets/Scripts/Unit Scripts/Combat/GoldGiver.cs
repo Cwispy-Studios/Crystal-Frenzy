8d7a125ad8c0230586952eac1a249587a6a43c18
Wong Zhihao <wzhao2679@hotmail.com>
2019-10-31 19:05:11 +0000

    Added gold from killing enemies

---
+++ Assets/Scripts/Unit Scripts/Combat/GoldGiver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GoldGiver : MonoBehaviour
+{
+  [SerializeField]
+  private int goldProvided = 0;
+
+  private void OnDestroy()
+  {
+    GameManager.resourceManager.SpendGold(-goldProvided);
+  }
+}
