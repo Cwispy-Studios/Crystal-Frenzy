3dc41d01011515b0345aaece121d68fa334c92a3
Wong Zhihao <wzhao2679@hotmail.com>
2019-11-13 21:32:37 +0000

    Added version number in main menu

---
+++ Assets/Scripts/UI/VersionNumber.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VersionNumber : MonoBehaviour
+{
+  private void Awake()
+  {
+    GetComponent<Text>().text = "Version " + Application.version.ToString();
+  }
+}
